ffe9dfc1da1f92890d28fe56a6f119faf3261eb6
Michael Pimentel <mtpjr88@gmail.com>
2019-04-02 21:06:56 -0700

    initial commit

---
+++ src/components/Link.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Link extends Component {
+	render() {
+		const {
+			link: { description, url }
+		} = this.props;
+		return (
+			<div>
+				<div>
+					{description} ({url})
+				</div>
+			</div>
+		);
+	}
+}
+
+export default Link;
